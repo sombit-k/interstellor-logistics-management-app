c867b516e6a86cf2f2ad2efbc54a7cdadda07633
Kartikeya-09 <kaushalkartikeya733@gmail.com>
2025-04-01 19:25:51 +0530

    implement waste management API with routes for identifying waste, generating return plans, completing undocking, and tracking waste items

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 
 import { connectDb } from "./lib/db.js"
+import wasteRoutes from "./routes/waste.js"
 
 dotenv.config()
 const app = express()
@@ -9,6 +10,7 @@ app.use(express.json())
 
 const PORT=process.env.PORT
 
+app.use("/api/waste", wasteRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
